47b126890a9e695324b9d7769dc1e35937d30580
Robson Lima <rsl.robson@gmail.com>
2020-06-15 11:14:08 -0300

    Criando projeto Kotlin e importando recursos da versão Java do projeto

---
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,14 @@
+package br.com.alura.financask.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.financask.R
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) : Unit {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
