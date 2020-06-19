78ab7fc26a2ef70d78e54f68cb6281f73fde8d5b
Robson Lima <rsl.robson@gmail.com>
2020-06-19 09:39:54 -0300

    Somando as transações de receita

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,10 +3,12 @@ package br.com.alura.financask.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.alura.financask.R
+import br.com.alura.financask.extension.formataParaBrasileiro
 import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
 import br.com.alura.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 import java.util.*
 
@@ -17,7 +19,20 @@ class ListaTransacoesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_lista_transacoes)
 
         val transacoes: List<Transacao> = transacoesDeExemplo()
+
+        adicionaReceitoNoResumo(transacoes)
+
         configuraLista(transacoes)
+    }
+
+    private fun adicionaReceitoNoResumo(transacoes: List<Transacao>) {
+        var totalReceita = BigDecimal.ZERO
+        for (transacao in transacoes) {
+            if (transacao.tipo == Tipo.RECEITA) {
+                totalReceita = totalReceita.plus(transacao.valor)
+            }
+        }
+        resumo_card_receita.text = totalReceita.formataParaBrasileiro()
     }
 
     private fun configuraLista(transacoes: List<Transacao>) {
